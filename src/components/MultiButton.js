41d995c4470b7dbc76ce161a29a1a74db691d2cf
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-27 21:56:22 +0300

    Completed

--- src/components/MultiButton.js
+++ src/components/MultiButton.js
@@ -6,10 +6,20 @@ function MultiButton() {
   }
 
   return (
+    //Becayse we want to pass in information to the event handle in this example,
+    //with the onclick event listener here we have to add another function and define it instead
+    //of just invoking the handleClick function
+
+    //        why?
+
+    // because when you don't provide a function definition
+    // the handleClick function is triggered by default when the page loads
+    // but when you do each button will now handle a function definitionthat
+    // will be invoked when the function is clicked
     <div>
-      <button onClick={handleClick}>Button 1</button>
-      <button onClick={handleClick}>Button 2</button>
-      <button onClick={handleClick}>Button 3</button>
+      <button onClick={() => handleClick(1)}>Button 1</button>
+      <button onClick={() => handleClick(2)}>Button 2</button>
+      <button onClick={() => handleClick(3)}>Button 3</button>
     </div>
   );
 }
